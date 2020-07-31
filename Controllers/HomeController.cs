5abefddee766af525d7a41e4092548d443e9bea8
lwandoThaGreat <66113508+lwandoThaGreat@users.noreply.github.com>
2020-07-31 11:33:40 +0200

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        { return View(); }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // there is a validation error                 
+                return View();
+            }
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+
+        public ViewResult Meals()
+        {
+            return View();
+        }
+        public ViewResult Booking()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/Meals.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PartyInvites.Models
+{
+    public class Meals : PageModel
+    {
+       
+            [BindProperty]
+            public string TypesOfMeal { get; set; }
+            public string[] TypesOfMeals = new[] { "Male", "Female", "Unspecified" };
+        
+   
+}
+}
+
